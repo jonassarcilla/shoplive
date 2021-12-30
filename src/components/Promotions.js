ebbe0765c8c3c7da927fe5c94bf2cf70e3ab26d8
DESKTOP-4EFEVVD\jrsar <jr.sarcilla07@gmail.com>
2021-12-30 10:35:50 +0800

    Add accessibility

--- src/components/Promotions.js
+++ src/components/Promotions.js
@@ -8,8 +8,8 @@ const Promotions = () => {
             <div className='flex flex-col container mx-auto pt-10 px-5'>
                 <h3 className='mb-5 text-lg text-center text-white font-bold'>Download ShopLive now and Get Promo Everyday</h3>
                 <div className='flex flex-row px-5 pb-9 gap-4'>
-                <button className='flex-auto rounded-lg bg-black'>&nbsp;</button>
-                <button className='flex-auto rounded-lg bg-black'>&nbsp;</button>
+                <button className='flex-auto rounded-lg bg-black' aria-label="Google PlayStore">&nbsp;</button>
+                <button className='flex-auto rounded-lg bg-black' aria-label="Apple Store">&nbsp;</button>
                 </div>
                 <div className='h-48 rounded-t-lg bg-white'>&nbsp;</div>
             </div>
